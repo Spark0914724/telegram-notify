f0124e894a485a99bbc4ffd6509bb36f18bf26eb
spark0914724 <sophia.Cls@outlook.com>
2026-05-06 02:32:28 +0300

    Define the message DTO

--- libs/shared/src/dto/message.dto.ts
+++ libs/shared/src/dto/message.dto.ts
@@ -1,6 +1,14 @@
+import { IsString, IsUUID, IsObject } from 'class-validator';
+
 export class MessageDto {
+  @IsUUID()
   messageId: string;
+
+  @IsString()
   eventType: string;
+
+  @IsObject()
   payload: Record<string, unknown>;
+
   timestamp: string;
 }
