3d406b68ddb0f245b39422b599a6372c3ecb1069
spark0914724 <sophia.Cls@outlook.com>
2026-05-06 05:53:56 +0300

    Initialize in Producer's main.ts

--- apps/producer/src/main.ts
+++ apps/producer/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ProducerModule } from './producer.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ProducerModule);
-  await app.listen(process.env.port ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Producer API')
+    .setDescription('Send messages to RabbitMQ queues')
+    .setVersion('1.0')
+    .addTag('messages')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
